05d0f12daec24837e1bfeab79d2ce8ec7eb9f4d4
jviictor <joaovictorrrr93@gmail.com>
2023-02-28 22:10:31 -0300

    testes-select-radio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -47,13 +47,40 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('button','Enviar').click()
         cy.get('.error').should('be.visible');
     })
-    it.only('envia o formuário com sucesso usando um comando customizado',function(){
+    it('envia o formuário com sucesso usando um comando customizado',function(){
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible');
     })
     it('Exercício extra 8',function(){
         cy.contains('button','Enviar').click()
+    })
+    it('select random',function(){
+        cy.get('select option')
+        .its('length',{log: false}).then(n=> {
+            cy.get('select').select(Cypress._.random(n-1))
+        })
+    })
+    it('seleciona um produto (YouTube) por seu texto',function(){
+        cy.get('#product').select('YouTube');
+        cy.get('#product').should('have.value','youtube')
+    })
+    it('seleciona um produto (Mentoria) por seu valor (value)',function(){
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria');
+    })
+    it('seleciona um produto (Blog) por seu índice',function(){
+        cy.get('#product').select(1).should('have.value', 'blog');;
+    })
+    it('marca o tipo de atendimento "Feedback"',function(){
+        cy.get(':nth-child(4) > input').check().should('have.value','feedback');
+    })
+    it.only('marca cada tipo de atendimento',function(){
+        cy.get('input[type="radio"]')
+        .should('have.length',3)
+        .each(function($radio){
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
     })
   })
   
